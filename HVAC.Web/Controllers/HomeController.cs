5dcfe7e33bb653e7b83d8718ca7e74a4e6bd4346
Eugeniu Nazarciuc <eugeniu.nazarciuc@isa.utm.md>
2025-03-03 13:48:49 +0200

    Обновил контроллер

---
+++ HVAC.Web/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HVAC.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            ViewBag.Message = "Our blog";
+            return View();
+        }
+
+        public ActionResult BlogDetails()
+        {
+            ViewBag.Message = "Blog details";
+            return View();
+        }
+
+        public ActionResult Car()
+        {
+            ViewBag.Message = "Our cars";
+            return View();
+        }
+
+        public ActionResult CarDetails()
+        {
+            ViewBag.Message = "Car details";
+            return View();
+        }
+    }
+}
